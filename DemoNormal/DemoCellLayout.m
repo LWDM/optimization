b26e7917bb684fe2397f8bcae71143fb55966376
luwangda <luwangda007@qq.com>
2020-05-03 15:07:00 +0800

    界面优化

--- DemoNormal/DemoCellLayout.m
+++ DemoNormal/DemoCellLayout.m
@@ -29,12 +29,60 @@
 }
 
 - (void)layout{
-   CGFloat sWidth = [UIScreen mainScreen].bounds.size.width;
-    
-      //计算title的宽和高
+    CGFloat sWidth = [UIScreen mainScreen].bounds.size.width;
+    //计算title的宽度
     CGFloat titleWidth = sWidth-180;
-    CGFloat titleHeight = [self calcLabelHeight:_dataModel.pvs fontSize:14 width:titleWidth];
+    CGFloat titleHeight = 0;
+    NSMutableParagraphStyle *paraStyle = [[NSMutableParagraphStyle alloc]init];
+    [paraStyle setLineSpacing:5];
+    NSDictionary *attributes = @{NSFontAttributeName: [UIFont systemFontOfSize:16],
+    NSForegroundColorAttributeName: [UIColor colorWithRed:26/255.0 green:26/255.0 blue:26/255.0 alpha:1]
+    ,NSParagraphStyleAttributeName: paraStyle
+    ,NSKernAttributeName:@0
+    };
+    NSMutableAttributedString *attrStr = [[NSMutableAttributedString alloc] initWithString:_dataModel.title attributes:attributes];
+    //计算title的高度
+    titleHeight = [self caculateAttributeLabelHeightWithString:attrStr width:titleWidth];
     self.titleRect = CGRectMake(15, 10, titleWidth, titleHeight);
+
+    //计算title的宽度
+    CGFloat titleWidth2 = sWidth-30;
+    CGFloat titleHeight2 = 0;
+    NSMutableParagraphStyle *paraStyle2 = [[NSMutableParagraphStyle alloc]init];
+    [paraStyle setLineSpacing:5];
+    NSDictionary *attributes2 = @{NSFontAttributeName: [UIFont systemFontOfSize:16],
+    NSForegroundColorAttributeName: [UIColor colorWithRed:26/255.0 green:26/255.0 blue:26/255.0 alpha:1]
+    ,NSParagraphStyleAttributeName: paraStyle2
+    ,NSKernAttributeName:@0
+    };
+    NSMutableAttributedString *attrStr2 = [[NSMutableAttributedString alloc] initWithString:_dataModel.title attributes:attributes2];
+    //计算title的高度
+    titleHeight2 = [self caculateAttributeLabelHeightWithString:attrStr2 width:titleWidth2];
+    self.titleRect2 = CGRectMake(15, 10, titleWidth2, titleHeight2);
+
+    //计算图片的位置
+    self.imagesArr = [[NSMutableArray alloc] init];
+    if (_dataModel.imglist.count < 3) {
+        self.imageRect = CGRectMake(CGRectGetMaxX(self.titleRect)+10, 15, 140, 120);
+    }else{
+        for (int i = 0; i<3; i++) {
+            CGRect imageRect = CGRectMake(((sWidth-50)/3 * i + i * 10 + 15), CGRectGetMaxY(self.titleRect2) + 10, 120, 100);
+            [self.imagesArr addObject:@(imageRect)];
+        }
+    }
+
+    //计算来源位置
+    CGFloat fromWidth = [self calcWidthWithTitle:_dataModel.from font:14];
+    CGFloat fromHeight = [self calcLabelHeight:_dataModel.from fontSize:14 width:fromWidth];
+    self.fromLabelRect = CGRectMake(15, CGRectGetMaxY(self.titleRect)+90, fromWidth, fromHeight);
+     
+
+    //计算阅读量位置
+    CGFloat pvsWidth = [self calcWidthWithTitle:_dataModel.pvs font:14];
+    CGFloat pvsHeight = [self calcLabelHeight:_dataModel.pvs fontSize:14 width:pvsWidth];
+    self.pvsLabelRect = CGRectMake(CGRectGetMaxX(self.fromLabelRect)+10, CGRectGetMaxY(self.titleRect)+90, pvsWidth, pvsHeight);
+
+    self.height = CGRectGetMaxY(self.pvsLabelRect);
 
 }
 
@@ -93,59 +141,7 @@
 /**
  
  CGFloat sWidth = [UIScreen mainScreen].bounds.size.width;
-    //计算title的宽度
-    CGFloat titleWidth = sWidth-180;
-    CGFloat titleHeight = 0;
-    NSMutableParagraphStyle *paraStyle = [[NSMutableParagraphStyle alloc]init];
-    [paraStyle setLineSpacing:5];
-    NSDictionary *attributes = @{NSFontAttributeName: [UIFont systemFontOfSize:16],
-    NSForegroundColorAttributeName: [UIColor colorWithRed:26/255.0 green:26/255.0 blue:26/255.0 alpha:1]
-    ,NSParagraphStyleAttributeName: paraStyle
-    ,NSKernAttributeName:@0
-    };
-    NSMutableAttributedString *attrStr = [[NSMutableAttributedString alloc] initWithString:_dataModel.title attributes:attributes];
-    //计算title的高度
-    titleHeight = [self caculateAttributeLabelHeightWithString:attrStr width:titleWidth];
-    self.titleRect = CGRectMake(15, 10, titleWidth, titleHeight);
     
-    //计算title的宽度
-    CGFloat titleWidth2 = sWidth-30;
-    CGFloat titleHeight2 = 0;
-    NSMutableParagraphStyle *paraStyle2 = [[NSMutableParagraphStyle alloc]init];
-    [paraStyle setLineSpacing:5];
-    NSDictionary *attributes2 = @{NSFontAttributeName: [UIFont systemFontOfSize:16],
-    NSForegroundColorAttributeName: [UIColor colorWithRed:26/255.0 green:26/255.0 blue:26/255.0 alpha:1]
-    ,NSParagraphStyleAttributeName: paraStyle2
-    ,NSKernAttributeName:@0
-    };
-    NSMutableAttributedString *attrStr2 = [[NSMutableAttributedString alloc] initWithString:_dataModel.title attributes:attributes2];
-    //计算title的高度
-    titleHeight2 = [self caculateAttributeLabelHeightWithString:attrStr2 width:titleWidth2];
-    self.titleRect2 = CGRectMake(15, 10, titleWidth2, titleHeight2);
-    
-    //计算图片的位置
-    self.imagesArr = [[NSMutableArray alloc] init];
-    if (_dataModel.imglist.count < 3) {
-        self.imageRect = CGRectMake(CGRectGetMaxX(self.titleRect)+10, 15, 140, 120);
-    }else{
-        for (int i = 0; i<3; i++) {
-            CGRect imageRect = CGRectMake(((sWidth-50)/3 * i + i * 10 + 15), CGRectGetMaxY(self.titleRect2) + 10, 120, 100);
-            [self.imagesArr addObject:@(imageRect)];
-        }
-    }
-    
-    //计算来源位置
-    CGFloat fromWidth = [self calcWidthWithTitle:_dataModel.from font:14];
-    CGFloat fromHeight = [self calcLabelHeight:_dataModel.from fontSize:14 width:fromWidth];
-    self.fromLabelRect = CGRectMake(15, CGRectGetMaxY(self.titleRect)+90, fromWidth, fromHeight);
-     
-    
-    //计算阅读量位置
-    CGFloat pvsWidth = [self calcWidthWithTitle:_dataModel.pvs font:14];
-    CGFloat pvsHeight = [self calcLabelHeight:_dataModel.pvs fontSize:14 width:pvsWidth];
-    self.pvsLabelRect = CGRectMake(CGRectGetMaxX(self.fromLabelRect)+10, CGRectGetMaxY(self.titleRect)+90, pvsWidth, pvsHeight);
-    
-    self.height = CGRectGetMaxY(self.pvsLabelRect);
  
  */
 
